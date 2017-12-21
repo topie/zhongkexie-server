7db92077defa3187ca10579fd44b3106b13dfae1
wjinliang <1028483257@qq.com>
2017-12-21 18:55:42 +0800

    手动执行insertData
    插入数据错误

--- src/main/resources/db/migration/V7_0_1__Cerate_d_common_statics_d_message.sql
+++ src/main/resources/db/migration/V7_0_1__Cerate_d_common_statics_d_message.sql
@@ -29,8 +29,6 @@ CREATE TABLE `d_common_statics` (
 -- ----------------------------
 -- Records of d_common_statics
 -- ----------------------------
-INSERT INTO `d_common_statics` VALUES ('message.content.template', '考评：${paperName}</br>开始时间:${startTime}</br>结束时间${endTime}', '${paperName},${startTime},${endTime}');
-INSERT INTO `d_common_statics` VALUES ('message.title.template', '考评通知${paperName}', '${paperName}');
 
 -- ----------------------------
 -- Table structure for d_message
---
+++ src/main/resources/db/migration/insertData.sql
@@ -0,0 +1,2 @@
+INSERT INTO `d_common_statics` VALUES ('message.content.template', '考评：${paperName}</br>开始时间:${startTime}</br>结束时间${endTime}', '${paperName},${startTime},${endTime}');
+INSERT INTO `d_common_statics` VALUES ('message.title.template', '考评通知${paperName}', '${paperName}');
