8b97d7493bb624199a760eed7b6d12f159a68783
zhaomin <zhaomin@mail.taiji.com.cn>
2017-12-21 10:32:46 +0800

    数据库提交

---
+++ src/main/resources/db/migration/V6_0_1__add_data.sql
@@ -0,0 +1,21 @@
+INSERT INTO d_score_answer VALUES ('2', '1', '1', '0', '2', '15.0000', '4', '3.0000', '');
+INSERT INTO d_score_answer VALUES ('3', '1', '1', '0', '1', '3.0000', '1', '0.0000', '');
+INSERT INTO d_score_answer VALUES ('4', '1', '1', '0', '3', '12.0000', '', '0.0000', '');
+
+INSERT INTO d_score_index VALUES ('1', '0', '组织支撑', '130.0000', '0');
+INSERT INTO d_score_index VALUES ('3', '1', '治理结构', '60.0000', '0');
+INSERT INTO d_score_index VALUES ('5', '3', '决策机构运行', '30.0000', '0');
+INSERT INTO d_score_index VALUES ('6', '5', '理事、常务理事履职尽责（理事会常务理事会的出席率）', '10.0000', '0');
+INSERT INTO d_score_index VALUES ('7', '5', '测试指标', '15.0000', '0');
+
+INSERT INTO d_score_item VALUES ('1', '6', '本年度召开理事会（  ）次', '1', '', '3.0000', '1');
+INSERT INTO d_score_item VALUES ('2', '6', '本次测试共（）', '1', '', '15.0000', '0');
+INSERT INTO d_score_item VALUES ('3', '7', '本次共召开（）次', '0', '', '12.0000', '0');
+
+INSERT INTO d_score_item_option VALUES ('1', '1', 'A', '0.0000', '0次', '1');
+INSERT INTO d_score_item_option VALUES ('2', '1', 'B', '1.0000', '1次及以上', '2');
+INSERT INTO d_score_item_option VALUES ('3', '2', 'A', '0.8000', '', '0');
+INSERT INTO d_score_item_option VALUES ('4', '2', 'B', '0.2000', '', '0');
+
+INSERT INTO d_score_paper VALUES ('1', '2017全国学会综合能力指标体系考评表', '{\"data\":[{\"indexId\":6,\"indexTitle\":\"理事、常务理事履职尽责（理事会常务理事会的出席率）\",\"items\":[{\"id\":2,\"itemType\":1,\"items\":[{\"id\":3,\"title\":\"A\"},{\"id\":4,\"title\":\"B\"}],\"score\":15.0000,\"title\":\"本次测试共（）\",\"type\":\"item\"},{\"id\":1,\"itemType\":1,\"items\":[{\"id\":1,\"title\":\"A\"},{\"id\":2,\"title\":\"B\"}],\"score\":3.0000,\"title\":\"本年度召开理事会（  ）次\",\"type\":\"item\"}],\"parentIndexTitle\":\"组织支撑>治理结构>决策机构运行>理事、常务理事履职尽责（理事会常务理事会的出席率）\"},{\"indexId\":7,\"indexTitle\":\"测试指标\",\"items\":[{\"id\":3,\"itemType\":0,\"items\":[],\"score\":12.0000,\"title\":\"本次共召开（）次\",\"type\":\"item\"}],\"parentIndexTitle\":\"组织支撑>治理结构>决策机构运行>测试指标\"}],\"title\":\"2017全国学会综合能力指标体系考评表\"}', '2017-12-19 19:37:53', '2017-12-19 19:37:53', '2017-12-19 19:37:59', '2017-12-21 09:45:04', '0', '0');
+INSERT INTO d_score_paper VALUES ('2', '2018全国学会综合能力指标体系考评表', '{\"data\":[{\"score\":130.0000,\"pid\":0,\"id\":1,\"title\":\"组织支撑\",\"type\":\"index\",\"items\":[{\"score\":60.0000,\"pid\":1,\"id\":3,\"title\":\"治理结构\",\"type\":\"index\",\"items\":[{\"score\":30.0000,\"pid\":3,\"id\":5,\"title\":\"决策机构运行\",\"type\":\"index\",\"items\":[{\"score\":10.0000,\"pid\":5,\"id\":6,\"title\":\"理事、常务理事履职尽责（理事会常务理事会的出席率）\",\"type\":\"index\",\"items\":[{\"id\":1,\"itemType\":1,\"items\":[{\"id\":1,\"title\":\"A\"},{\"id\":2,\"title\":\"B\"}],\"score\":3.0000,\"title\":\"本年度召开理事会（  ）次\",\"type\":\"item\"}]}]}]}]}],\"title\":\"2018全国学会综合能力指标体系考评表\"}', '2017-12-20 15:00:21', '2017-12-20 15:00:21', '2017-12-20 15:00:27', '2017-12-20 15:00:27', '0', '0');
