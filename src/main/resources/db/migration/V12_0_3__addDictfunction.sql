badce221c649f438b33c199d84a567f951cb73e4
wjinliang <1028483257@qq.com>
2018-01-18 16:47:09 +0800

    s

---
+++ src/main/resources/db/migration/V12_0_3__addDictfunction.sql
@@ -0,0 +1,2 @@
+INSERT INTO `d_function` (`id`, `parent_id`, `function_name`, `display`, `state`, `icon`, `action`, `function_desc`, `insert_time`, `update_time`) VALUES ('20', '2', '字典管理', '1', '1', 'fa fa-book', '/api/core/dict/page', '6', '2018-01-17 17:16:12', '2018-01-17 17:16:12');
+INSERT INTO `d_role_function` (`function_id`, `role_id`) VALUES ('20', '1');
